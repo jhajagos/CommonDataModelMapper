165619f8d2cdc8458d6223a24d5a3ade5f64d878
janos <risk.limits@gmail.com>
2018-04-28 11:04:32 -0400

    Trying to deal with pool errors

--- omop_cdm/concept_server/concept_server.py
+++ omop_cdm/concept_server/concept_server.py
@@ -28,59 +28,59 @@ def convert_to_result_dict(result):
 
 def find_concept_id_by_code(vocabulary, code):
 
-    connection = connect_to_database()
+    with connect_to_database() as connection:
 
-    meta_data = sa.MetaData(connection)
-    meta_data.reflect()
+        meta_data = sa.MetaData(connection)
+        meta_data.reflect()
 
-    concept_obj = meta_data.tables["concept"]
+        concept_obj = meta_data.tables["concept"]
 
-    concept_sel_obj = concept_obj.select().where(
-        sa.and_(concept_obj.columns["vocabulary_id"] == vocabulary,
-                concept_obj.columns["concept_code"] == code))
+        concept_sel_obj = concept_obj.select().where(
+            sa.and_(concept_obj.columns["vocabulary_id"] == vocabulary,
+                    concept_obj.columns["concept_code"] == code))
 
-    cursor = connection.execute(concept_sel_obj)
-    results = list(cursor)
+        cursor = connection.execute(concept_sel_obj)
+        results = list(cursor)
 
-    concept_relation_obj = meta_data.tables["concept_relationship"]
+        concept_relation_obj = meta_data.tables["concept_relationship"]
 
-    if len(results):
-        result = results[0]
+        if len(results):
+            result = results[0]
 
-        result_dict = convert_to_result_dict(result)
+            result_dict = convert_to_result_dict(result)
 
-        result_dict["mapped_standard_concept"] = {}
-        if result_dict["standard_concept"] == "S":
-            result_dict["is_standard_concept"] = True
+            result_dict["mapped_standard_concept"] = {}
+            if result_dict["standard_concept"] == "S":
+                result_dict["is_standard_concept"] = True
+            else:
+                result_dict["is_standard_concept"] = False
+
+                concept_rel_select_obj = concept_relation_obj.select().where(
+                    sa.and_(concept_relation_obj.columns["concept_id_1"] == result_dict["concept_id"],
+                            concept_relation_obj.columns["relationship_id"] == "Maps to"
+                            )
+                )
+
+                cursor = connection.execute(concept_rel_select_obj)
+                result_relationships = list(cursor)
+
+                if len(result_relationships):
+                    result_relationship = result_relationships[0]
+                    mapped_concept_id = result_relationship.concept_id_2
+
+                    concept_mapped_sel_obj = concept_obj.select().where(
+                        concept_obj.columns["concept_id"] == mapped_concept_id)
+                    cursor = connection.execute(concept_mapped_sel_obj)
+                    result_concept_mapped = list(cursor)
+
+                    if len(result_concept_mapped):
+                        result_mapped_dict = convert_to_result_dict(result_concept_mapped[0])
+                        result_dict["mapped_standard_concept"] = result_mapped_dict
+
+            return result_dict
+
         else:
-            result_dict["is_standard_concept"] = False
-
-            concept_rel_select_obj = concept_relation_obj.select().where(
-                sa.and_(concept_relation_obj.columns["concept_id_1"] == result_dict["concept_id"],
-                        concept_relation_obj.columns["relationship_id"] == "Maps to"
-                        )
-            )
-
-            cursor = connection.execute(concept_rel_select_obj)
-            result_relationships = list(cursor)
-
-            if len(result_relationships):
-                result_relationship = result_relationships[0]
-                mapped_concept_id = result_relationship.concept_id_2
-
-                concept_mapped_sel_obj = concept_obj.select().where(
-                    concept_obj.columns["concept_id"] == mapped_concept_id)
-                cursor = connection.execute(concept_mapped_sel_obj)
-                result_concept_mapped = list(cursor)
-
-                if len(result_concept_mapped):
-                    result_mapped_dict = convert_to_result_dict(result_concept_mapped[0])
-                    result_dict["mapped_standard_concept"] = result_mapped_dict
-
-        return result_dict
-
-    else:
-        return {}
+            return {}
 
 
 @app.route("/code/<vocabulary>/<code>")
